cfb40e164026a9000d66e0e38d5e51bb01b87514
Daniel Espinal <dancancode@protonmail.com>
2023-01-30 10:33:58 -0500

    wrote smart contract, delpy script, and used goerli faucet

---
+++ smart-contract/hardhat.config.js
@@ -0,0 +1,14 @@
+//require("@nomicfoundation/hardhat-toolbox");
+
+require("@nomiclabs/hardhart-waffle");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  network: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/ux69TEW26IxG6ELTsL1_VVfN20Oe7GJW",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};

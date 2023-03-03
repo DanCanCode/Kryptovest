de77ed16118e1bf31a9a17db3987c1f729d90405
Daniel Espinal <dancancode@protonmail.com>
2023-03-03 02:12:31 -0500

    setup postgres db on railway

--- server/db/database.js
+++ server/db/database.js
@@ -1,14 +1,18 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
+// const sequelize = new Sequelize(
+//   "kryptovest",
+//   process.env.DB_USERNAME,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//   }
+// );
+
 const sequelize = new Sequelize(
-  "kryptovest",
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-  }
+  `postgresql://postgres:${process.env.DB_PASSWORD}@containers-us-west-125.railway.app:7875/railway`
 );
 
 module.exports = sequelize;

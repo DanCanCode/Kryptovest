ab600fcccadd1ef444ce200ec6461ec6042d0e6f
Daniel Espinal <dancancode@protonmail.com>
2023-03-05 22:03:54 -0500

    added unique key to nav

--- my-app/client/src/components/Navbar.jsx
+++ my-app/client/src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
 
   useEffect(() => {
     dispatch(me());
-  }, [window.localStorage]);
+  }, []);
 
   const handleLogout = (e) => {
     e.preventDefault();
@@ -37,7 +37,10 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+    <nav
+      key={userData.id}
+      className="w-full flex md:justify-center justify-between items-center p-4"
+    >
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
         <img
           onClick={() => navigate("/")}
